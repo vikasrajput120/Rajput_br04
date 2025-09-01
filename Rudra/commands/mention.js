9e8e4743907293093b1181fb97e65ca645742c6a
vikasrajput120 <vk594650659@gmail.com>
2025-09-01 18:54:56 +0530

    Update mention.js

--- Rudra/commands/mention.js
+++ Rudra/commands/mention.js
@@ -1,22 +1,22 @@
 module.exports.config = {
   name: "goiadmin",
-  version: "1.0.0",
+  version: "1.0.0-beta-fixbyDungUwU",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "ZyrosGenZ-fixbyDungUwU",
   description: "Bot will rep ng tag admin or rep ng tagbot ",
   commandCategory: "Other",
   usages: "",
   cooldowns: 1
 };
 module.exports.handleEvent = function({ api, event }) {
-  if (event.senderID !== "61550558518720") {
-    var aid = ["61550558518720"];
+  if (event.senderID !== "100081181726230","100090065722539") {
+    var aid = ["100081181726230","100090065722539"];
     for (const id of aid) {
     if ( Object.keys(event.mentions) == id) {
-      var msg = ["Wo Busy H mujhe Bolo Kya Bolna H?", "Kya Hua Boss ko q Bula Rhe Ho?", "Wo Shayad Busy hoga", "  Bhai Rudra Toh Chala gaya"];
+      var msg = ["Abee Mere se baat Kar ..use mention kyu kar rhe ho😒", "Kya Hua Boss ko q Bula Rhe Ho?", "Wo Shayad Busy h😐🙄", "Tujhe sunai nhi deta mere admin ko tang na kr😒😒", "Mere boss ko tang naa karo", "Abee Bola na mere Admin ko bar bar mention mat karo ek bar me nahi smjh nahi ata", "Kya kaam hai mujhe bol", "Ek Baar me samjh ni ata , kyu mention kar rhe ho use" , "Mere Boss ko tang mt kiya kro smjhe😒"];
       return api.sendMessage({body: msg[Math.floor(Math.random()*msg.length)]}, event.threadID, event.messageID);
     }
     }}
 };
 module.exports.run = async function({}) {
-        }
+}
